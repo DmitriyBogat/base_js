5b9b2bdfd5c9857311756f4e1591115a21a3e2a4
Dmitriy_Bogat <bogat.dv@mail.ru>
2022-06-29 16:58:31 +0300

    Исполнена лаб 2

--- lab8/1 текст вместо картинки/app.js
+++ lab8/1 текст вместо картинки/app.js
@@ -4,8 +4,8 @@
 // 1.1 затем проитерируйтесь по кнопкам и каждой из
 // них добавьте обработчик клика - функцию handleClick
 let buttons = document.querySelectorAll("button");
-buttons.forEach(button =>{
-    button.addEventListener("click",event =>handleClick(event))
+buttons.forEach(button => {
+    button.addEventListener("click", event => handleClick(event))
 })
 
 /**
@@ -13,6 +13,7 @@ buttons.forEach(button =>{
  * функции для показа или скрытия текста о товаре.
  * @param {MouseEvent} clickedButtonEvent 
  */
+
 function handleClick(clickedButtonEvent) {
     const cardNode = clickedButtonEvent.currentTarget.parentNode
     // 2. из объекта события получите ссылку на .product и
@@ -21,19 +22,19 @@ function handleClick(clickedButtonEvent) {
 
     // 3. создайте литерал объекта со следующими свойствами:
     const card = {
-        wrap: cardNode , // здесь элемент с классом .product
-        img: cardNode.HTMLImageElement , // здесь картинка внутри .product
-        productName: cardNode.get , // здесь .productName, который внутри .product
-        button: cardNode. , // здесь button, который внутри .product
+        wrap: cardNode, // здесь элемент с классом .product
+        img: cardNode.querySelector("img"), // здесь картинка внутри .product
+        productName: cardNode.querySelector(".productName"), // здесь .productName, который внутри .product
+        button: cardNode.querySelector("button"), // здесь button, который внутри .product
     };
-
+    let text = card.button.textContent;
     // 4. получаем текст на кнопке, которая внутри .product
-    
-    
-    if () { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
+
+
+    if (text == "Подробнее") { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
         // 4.2 если да, то передаем объект card в функцию showMoreText
         showMoreText(card);
-    } else if () { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
+    } else if (text == "Отмена") { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
         // 4.4 если да, то передаем объект card в функцию hideMoreText
         hideMoreText(card);
     }
@@ -48,12 +49,15 @@ function handleClick(clickedButtonEvent) {
  * @param {HTMLButtonElement} card.button
  */
 function hideMoreText(card) {
+    card.img.style.display = "block";
+    card.wrap.querySelector(".desc").remove();
+    card.button.textContent = "Подробнее";
     // 5. картинке внутри .product ставим стиль display: block
-    
+
     // 5.1 внутри .product находим элемент с классом .desc и удаляем его
-    
+
     // 5.2 кнопке, которая внутри .product ставим текст "Подробнее"
-    
+
 }
 
 /**
@@ -65,12 +69,16 @@ function hideMoreText(card) {
  * @param {HTMLButtonElement} card.button 
  */
 function showMoreText(card) {
+    card.img.style.display = "none";
+    let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis fuga repellendus harum magnam aliquam labore suscipit, dolore, iusto fugit aut ducimus eveniet repellat! Saepe aliquid autem aperiam et dignissimos commodi.";
+    card.productName.insertAdjacentHTML("afterend", `<div class='desc'>${text}<div>`);
+    card.button.textContent = "Отмена";
     // 6. картинке внутри .product ставим display: none
-    
+
     // 6.1 сохраняем произвольный текст в переменную
-    
+
     // 6.2 внутри .product есть .productName, после него вставляем div.desc и текстом из переменной из п. 6.1
-    
+
     // 6.3 внутри .product у кнопки текст ставим "Отмена"
-    
+
 }
--- lab8/3 шахматная доска (где описан каждый шаг)/scripts/app.js
+++ lab8/3 шахматная доска (где описан каждый шаг)/scripts/app.js
@@ -27,14 +27,14 @@ let app = {
         let whitePawnsRow = document.querySelectorAll();
         for (let i = 0; i < whitePawnsRow.length; i++) {
             // 6.1 вставляем в текущий тег td белую пешку
-            whitePawnsRow[i].innerHTML = this.getFigure(, );
+            whitePawnsRow[i].innerHTML = this.getFigure(,);
         }
 
         // 6.2 получаем все теги td из 2 линии игрового поля, туда мы будем вставлять черные пешки
         let blackPawnsRow = document.querySelectorAll();
         for (let i = 0; i < blackPawnsRow.length; i++) {
             // 6.3 вставляем в текущий тег td черную пешку
-            blackPawnsRow[i].innerHTML = this.getFigure(, );
+            blackPawnsRow[i].innerHTML = this.getFigure("pawn", "blackFigure");
         }
     },
 
@@ -48,7 +48,7 @@ let app = {
             // coordRow и coordCol из массива positions
             let cell = document.querySelector(`[data-rownum="${positions[i].}"][data-colchar="${positions[i].}"]`);
             // 5.2 из массива positions, для текущего объекта по индексу i, передаем название фигуры, затем цвет+"Figure"
-            let figure = this.getFigure(positions[i]., positions[i].+"Figure");
+            let figure = this.getFigure(positions[i]., positions[i].+ "Figure");
             // 5.3 в innerHTML текущей ячейки ставим figure
             cell. = ;
         }
@@ -72,10 +72,10 @@ let app = {
      */
     generateBoard() {
         // 3. создаем переменную board и присваиваем ей пустую строку, в ней будет храниться разметка игрового поля в виде строки
-        
+
         // 3.1 создаем переменную rowStartWithColor, в которой укажем, что линия на игровом поле будет начинаться с белого цвета
         // т.е. присваиваем строку "white"
-        
+
         // 3.2 в цикле перебираем все строки из конфига
         for (let i = 0; i < ; i++) {
             // 3.3 здесь в переменной row в виде строки будет храниться разметка для линии на поле (тег tr),
@@ -87,12 +87,12 @@ let app = {
                 // которой передали цвет, с которого начинается линия на поле, а также this.config.rows[i]
                 row = this.generateRow(, this.config.rows[i]);
                 // 3.6 затем меняем rowStartWithColor на "black"
-                
+
             } else {
                 // 3.7 делаем то же что и в п. 1.5
                 row = this.generateRow(, this.config.rows[i]);
                 // 3.8 меняем rowStartWithColor на "white"
-                
+
             }
             // 3.9 в board дописываем с помощью +=, то что получилось в переменной row
             board += row;
@@ -111,84 +111,84 @@ let app = {
      */
     generateRow(startWithColor, rowNum) {
         // 2. создаем переменную currentColorClass, в которую пишем startWithColor
-        
+
         // 2.1 создаем переменную row и присваиваем ей пустую строку, в ней будет храниться набор тегов td в виде
         // строки, т.е. это будет одна линия игрового поля
-        
+
         // 2.2 в цикле перебираем массив cols из config
         for (let i = 0; i < ; i++) {
             // 2.3 создаем переменную field, в которую записываем пустую строку, это будет тег td, ячейка игрового поля
-            
+
             // 2.4 проверяем, равняется ли currentColorClass строке "white"
             if () {
                 // 2.5 в переменную field пишем результат вызова функции this.generateField, которой передаем цвет ячейки игрового
                 // поля в виде строки, затем номер строки игровой доски в виде числа, последним аргументом передаем this.config.cols[i]
                  = this.generateField(, , this.config.cols[i]);
-                // 2.6 присваиваем в currentColorClass строку "black"
-                
-            } else {
+            // 2.6 присваиваем в currentColorClass строку "black"
+
+        } else {
                 // 2.7 делаем то же что и в п. 2.5, только первым параметром передаем "black"
                  = this.generateField(, , this.config.cols[i]);
-                // 2.8 переменной currentColorClass присваиваем строку "white"
-                
-            }
-            // 2.9 в переменную row дописываем с помощью += то что получилось в field
-            row += field;
+    // 2.8 переменной currentColorClass присваиваем строку "white"
+
+}
+// 2.9 в переменную row дописываем с помощью += то что получилось в field
+row += field;
         }
-        return `<tr>${row}</tr>`;
+return `<tr>${row}</tr>`;
     },
 
-    /**
-     * Метод генерирует ячейку (тег td) с нужным классом цвета
-     * и координатами на поле.
-     * @param {string} color класс цвета ячейки, м.б. "white", "black".
-     * @param {number} rowNum номер строки игровой доски.
-     * @param {string} colChar буква колонки игровой доски.
-     * @returns {string} html-разметка с заполненными атрибутами координат и классом цвета.
-     */
-    generateField(color, rowNum, colChar) {
-        // 1. вставьте нужные значения в атрибуты
-        return `<td data-rownum="${}" data-colchar="${}" class="${}"></td>`;
-    },
+/**
+ * Метод генерирует ячейку (тег td) с нужным классом цвета
+ * и координатами на поле.
+ * @param {string} color класс цвета ячейки, м.б. "white", "black".
+ * @param {number} rowNum номер строки игровой доски.
+ * @param {string} colChar буква колонки игровой доски.
+ * @returns {string} html-разметка с заполненными атрибутами координат и классом цвета.
+ */
+generateField(color, rowNum, colChar) {
+    // 1. вставьте нужные значения в атрибуты
+    return `<td data-rownum="${}" data-colchar="${}" class="${}"></td>`;
+},
 
-    /**
-     * Метод вставляет на существующую доску колонку 
-     * слева, с указанием номера строки.
-     */
-    insertRowsNumbers() {
-        // 7. получаем все теги tr
-        let trs = document.querySelectorAll();
-        // 7.1 перебираем эти теги в цикле
-        for (let i = 0; i < ; i++) {
-            // 7.2 создаем тег td
-            let td = ;
-            // 7.3 в текущий тег td в innerText вставляем номер строки из this.config.rows
-            td. = this.config.rows[ ];
-            // 7.4 получившийся тег td вставляем в текущую строку (тег tr)
-            trs[ ].insertAdjacentElement("afterbegin", td);
-        }
-    },
+/**
+ * Метод вставляет на существующую доску колонку 
+ * слева, с указанием номера строки.
+ */
+insertRowsNumbers() {
+    // 7. получаем все теги tr
+    let trs = document.querySelectorAll();
+    // 7.1 перебираем эти теги в цикле
+    for (let i = 0; i < ; i++) {
+        // 7.2 создаем тег td
+        let td = ;
+        // 7.3 в текущий тег td в innerText вставляем номер строки из this.config.rows
+        td. = this.config.rows[];
+        // 7.4 получившийся тег td вставляем в текущую строку (тег tr)
+        trs[].insertAdjacentElement("afterbegin", td);
+    }
+},
 
-    /**
-     * Метод создает строку (tr) с названиями колонок в виде букв,
-     * а также в начале вставляет пустую ячейку, которая идет под
-     * цифрами.
-     */
-    insertColsChars() {
-        // 8. создаем тег tr
-        let tr = ;
-        // 8.1 затем в innerHTML тега tr дописываем пустой тег td
-        tr. += '';
-        // 8.2 в цикле перебираем колонки из this.config.cols
-        for (let i = 0; i < ; i++) {
-            // 8.3 в innerHTML с помощью += дописываем тег td c буквой текущей колонки из this.config.cols
-            tr. += `<td>${this.config.cols[ ]}</td>`;
-        }
-        // 8.4 получаем тег tbody
-        let tbody = ;
-        // 8.5 и в него перед </tbody> вставляем значение из переменной tr
-        tbody.insertAdjacentElement(, );
-    },
+/**
+ * Метод создает строку (tr) с названиями колонок в виде букв,
+ * а также в начале вставляет пустую ячейку, которая идет под
+ * цифрами.
+ */
+insertColsChars() {
+    // 8. создаем тег tr
+    let tr = ;
+    // 8.1 затем в innerHTML тега tr дописываем пустой тег td
+    tr. += '';
+    // 8.2 в цикле перебираем колонки из this.config.cols
+    for (let i = 0; i < ; i++) {
+        // 8.3 в innerHTML с помощью += дописываем тег td c буквой текущей колонки из this.config.cols
+        tr. += `<td>${this.config.cols[]}</td>`;
+    }
+    // 8.4 получаем тег tbody
+    let tbody = ;
+    // 8.5 и в него перед </tbody> вставляем значение из переменной tr
+    tbody.insertAdjacentElement(,);
+},
 }
 
 app.run();
